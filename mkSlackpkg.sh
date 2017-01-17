3901d0f8907af06b98d38461a108e1517ff840ec
mrush <m@root.dance>
2017-01-17 02:59:35 -0700

    .

--- mkSlackpkg.sh
+++ mkSlackpkg.sh
@@ -6,10 +6,10 @@ arch="noarch"
 plvl="0"
 tag="mrush"
 pkg="${name}-${ver}-${arch}-${plvl}_${tag}"
-dest="/var/www/htdocs/mirror/slack/slackware-packages/"
+dest="/var/www/htdocs/mirror/slack/slackware-packages"
 
-mkdir $dest/$pkg
+mkdir "$dest/$pkg"
 #find . -type d -maxdepth 1 -mindepth 1 \! -name ".git*" |xargs -I% cp -vR % ../$pkg
-find . -type d -maxdepth 1 -mindepth 1 \! -name ".git*" -exec cp -vR {} ../$pkg \;
-makepkg -c y ../$pkg.txz
+find . -type d -maxdepth 1 -mindepth 1 \! -name ".git*" -exec cp -vR {} $dest/$pkg \;
+makepkg -c y $dest/$pkg.txz
 rm -rf $dest/$pkg
