82c1419bb53e09c907a87de975f91f0db01e17d6
M. Rush <m@root.dance>
2017-01-19 23:25:48 +0000

    Testing lab support in conf.sh and create.sh, and remove.sh, unremove.sh, destroy.sh...

--- usr/local/lib/vmlab/destroy.sh
+++ usr/local/lib/vmlab/destroy.sh
@@ -5,7 +5,21 @@ destroy () {
   [ -e "$trashpath/$guest.conf" ] && source $trashpath/$guest.conf
   saveuuid=$uuid
   
-  #  if the guest is in the trash, delete it
-  [ ! -e $trashpath/?(new.)$guest.img ] && echo "Guest is not in trash. Remove first" && return 1
-  rm -f $trashpath/?(*)$guest.{img,conf}
+  if [[ $guest =~ / ]]; then
+    lab="$(echo $guest |rev |cut -d / -f2- |rev)"
+    gName="$(echo $guest |rev |cut -d / -f1 |rev)"
+
+    # remove from trash
+    [ ! -e $trashpath/$lab/?(new.)$gName.img ] && echo "Guest is not in trash. Remove first" && return 1
+    rm -f $trashpath/$lab/?(*)$gName.{img,conf}
+
+    # if it's the only guest in the lab directory within the trash, delete the directory as well
+    count=$(find $trashpath/$lab \! -name ".*" |wc -l)
+    if [[ $count -eq 1 ]]; then
+      rm -rf $trashpath/$lab
+    fi
+  else
+    [ ! -e $trashpath/?(new.)$guest.img ] && echo "Guest is not in trash. Remove first" && return 1
+    rm -f $trashpath/?(*)$guest.{img,conf}
+  fi
 }
--- usr/local/lib/vmlab/metadata/metadata.running.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-running () {
-  ps aux | grep qemu | grep -v qemu
-  NUMBER=`ps aux | grep qemu | grep -v qemu | wc -l`
-  echo "$NUMBER guests running"
-}
