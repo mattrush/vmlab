d231b44f9f5d99c73aa7136b73d638829464ae07
M. Rush <m@root.dance>
2017-01-21 23:50:37 +0000

    Updated install.sh to work with lab dir# with '#' will be ignored, and an empty message aborts the commit.

--- usr/local/lib/vmlab/install.sh
+++ usr/local/lib/vmlab/install.sh
@@ -5,14 +5,22 @@ install_ () {
   # restrict action by run state
   [ -n "$runflag" ] && echo "Guest is running. Halt first" && return 1	
 
-  # make sure guest is new
-  [ ! -e "$imagepath/new.$guest.img" ] && echo "Guest is not new. Cannot install" && return 1
+  if [[ -n $lab ]]; then
+    # make sure guest is new
+    [ ! -e "$imagepath/$lab/new.$gName.img" ] && echo "Guest is not new. Cannot install" && return 1
+    qString='-hda "$imagepath/$lab/new.$gName".img \'
+  else
+    # make sure guest is new
+    [ ! -e "$imagepath/new.$guest.img" ] && echo "Guest is not new. Cannot install" && return 1
+    qString='-hda "$imagepath/new.$guest".img \'
+  fi
 
   # if chroot directory doesn't exist, create it
   [ ! -d "$chroot" ] && mkdir -p "$chroot"
 
   # boot guest to installation media
   qemu-kvm \
+    "$qString"
     -hda "$imagepath/new.$guest".img \
     -m "$mem" \
     -device "$nicdriver",netdev="$lab.$uuid",mac="$mac" \
@@ -32,5 +40,9 @@ install_ () {
 
   # if success, rename guest image to indicate it's not new anymore
   #(ps aux |grep -v grep |grep qemu-kvm |grep "$guest") && 
-  es=$?; [ "$es" == 0 ] && mv $imagepath/{new.,}$guest.img && return 0 || return 1
+  [[ -n $lab ]] && {
+    es=$?; [ "$es" == 0 ] && mv $imagepath/{new.,}$guest.img && return 0 || return 1
+  } || {
+    es=$?; [ "$es" == 0 ] && mv $imagepath/$lab/{new.,}$gName.img && return 0 || return 1
+  }
 }
