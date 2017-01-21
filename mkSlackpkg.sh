543506a887bdde6002ea27e8381429e02724f26e
M. Rush <m@root.dance>
2017-01-21 23:14:11 +0000

    Added old single-node metadata scripts.

--- mkSlackpkg.sh
+++ mkSlackpkg.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 name="vmlab"
-ver="0.1.8.1"
+ver="0.1.8.11"
 arch="noarch"
 plvl="0"
 tag="mrush"
 pkg="${name}-${ver}-${arch}-${plvl}_${tag}"
-dest="/var/www/htdocs/mirror/slack/slackware-packages"
+dest="../"
 
 mkdir "$dest/$pkg"
 #find . -type d -maxdepth 1 -mindepth 1 \! -name ".git*" |xargs -I% cp -vR % ../$pkg
